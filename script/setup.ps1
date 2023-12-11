2127c996d0f2c492f398e4b6b9c0b1df7e6dc902
Christopher Larson <kergoth@gmail.com>
2023-12-11 04:53:37 +0000

    Add message about installing scoop to setup.ps1
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- script/setup.ps1
+++ script/setup.ps1
@@ -8,14 +8,14 @@ Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 
 # Install chezmoi if necessary
 if (-Not (Get-Command chezmoi -ErrorAction SilentlyContinue)) {
-    Write-Output "Installing chezmoi"
-
     if (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+        Write-Output "Installing scoop"
         Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
     }
 
     RefreshEnvPath
 
+    Write-Output "Installing chezmoi"
     scoop install chezmoi
 }
 
