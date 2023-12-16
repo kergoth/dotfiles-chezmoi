066279cd8e3bfa56562f6eb2c1c250e4a08a1750
Christopher Larson <kergoth@gmail.com>
2023-12-16 04:14:33 +0000

    Split bootstrap out of setup
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- script/setup.ps1
+++ script/setup.ps1
@@ -1,23 +1,6 @@
-function RefreshEnvPath {
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
-        + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-}
+Join-Path $PSScriptRoot "bootstrap.ps1" | Invoke-Expression
 
-# Use RemoteSigned execution policy for PowerShell. Needed for scoop, etc.
 Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-
-# Install chezmoi if necessary
-if (-Not (Get-Command chezmoi -ErrorAction SilentlyContinue)) {
-    if (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-        Write-Output "Installing scoop"
-        Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-    }
-
-    RefreshEnvPath
-
-    Write-Output "Installing chezmoi"
-    scoop install chezmoi
-}
 
 # Set path to this dotfiles repo
 $env:DOTFILES_DIR = $PSScriptRoot | Split-Path -Parent
