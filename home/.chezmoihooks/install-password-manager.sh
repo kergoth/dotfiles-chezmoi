dfa6b71f98264cbe1774b7b63e8fb067279ce32d
Christopher Larson <kergoth@gmail.com>
2023-12-16 00:04:38 +0000

    Use a hook to install 1password-cli
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

---
+++ home/.chezmoihooks/install-password-manager.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$HOME/.brew}"
+PATH="$HOMEBREW_PREFIX/bin:$HOME/.local/bin:$PATH"
+
+if command -v op >/dev/null 2>&1; then
+    exit
+fi
+
+case "$(uname -s)" in
+Darwin)
+    if ! [ -x "$HOMEBREW_PREFIX/bin/brew" ]; then
+        echo >&2 "Installing homebrew"
+        "$CHEZMOI_COMMAND_DIR/scripts/macos/install-brew" -s "$HOMEBREW_PREFIX" || {
+            rm -rf "$HOMEBREW_PREFIX"
+            echo >&2 "Failed to install homebrew"
+            exit 1
+        }
+    fi
+
+    if command -v brew >/dev/null 2>&1; then
+        brew install 1password-cli
+    fi
+    ;;
+Linux)
+    case "$(uname -m)" in
+    x86_64)
+        ARCH=amd64
+        ;;
+    aarch64)
+        ARCH=arm64
+        ;;
+    *)
+        echo >&2 "Error: unsupported arch from uname -m: $(uname -m)"
+        exit 1
+        ;;
+    esac
+    wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.24.0/op_linux_${ARCH}_v2.24.0.zip" -O op.zip &&
+        unzip -d ~/.local/bin op.zip op
+    chmod +x ~/.local/bin/op
+    ;;
+*)
+    echo "Error: unsupported OS: $(uname -s)"
+    exit 1
+    ;;
+esac
